39c6b4c6bde0a357c4715b1f320de40b87d01694
FrhaanSaqib <developer.farhansaqib@gmail.com>
2023-07-16 12:25:33 +0500

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Farhan Saqib on 16/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
