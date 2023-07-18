d7093a0595a2cbe648fa0223100c118344fc4273
FrhaanSaqib <developer.farhansaqib@gmail.com>
2023-07-18 14:03:29 +0500

    Basic Project Skeleton Created
    //Additions
    1. Added Folders Structure For App Delegate and SceneDelegate
    2. Created ViewControllers Folder and Added NewsListVC
    3. Lined NewsListVc to main Storyboard
    
    //Deletions
    Removed Default View Controller file

--- NewsApp/Controllers/NewsListVC.swift
+++ NewsApp/Controllers/NewsListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsListVC.swift
 //  NewsApp
 //
 //  Created by Farhan Saqib on 16/07/2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
