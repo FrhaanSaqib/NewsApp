0fa5ab8519999182190a43a9a7738ee273ef64d6
FrhaanSaqib <developer.farhansaqib@gmail.com>
2023-07-18 14:10:15 +0500

    Created Empty NewsCell
    //Additions
    1. Created NewsCell
    2. News Cell along with its nib file
    3. Nib file will hold the view of the cell in near future.

---
+++ NewsApp/Views/NewsCell.swift
@@ -0,0 +1,20 @@
+//
+//  NewsCell.swift
+//  NewsApp
+//
+//  Created by Farhan Saqib on 18/07/2023.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
